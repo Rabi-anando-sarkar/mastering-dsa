795077c3aa444ae9f7cd06de1f1770f4e2cb64cc
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2025-06-13 09:29:52 +0530

    Find the K-th Character in String Game I | Day 1

---
+++ Question_4/Day_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string genWord(string word, int k) {
+    if(word.size() > k) return word;
+
+    string temp;
+
+    for(int i=0 ; i<word.size() ;i++) {
+        if(word[i] == 'z') {
+            temp.append(1,'a');
+        } else {
+            temp.append(1, word[i]+1);
+        }
+    }
+
+    return genWord(word+temp, k);
+}
+
+int main() {
+    int k = 5;
+    string word = "a";
+
+    word = genWord(word, k-1);
+
+    cout << "The kth Element is : " << word[k-1] << endl;
+    return 0;
+}
