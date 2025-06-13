2d6bd65b91eec83a6b44e221ca1c256638c9954f
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2025-06-13 09:19:13 +0530

    Power of Four | Day 1

---
+++ Question_2/Day_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class PowerOfFour {
+    public:
+        bool divisionMethod(int num) {
+            if(num == 1) return true;
+            if(num <= 0 || num%4 != 0) return false;
+            return divisionMethod(num/4);
+        }
+
+        bool multiplicationMethod(int value, int num) {
+            if(value == num) return true;
+            if(value > num || num < 1) return false;
+            return multiplicationMethod(value*4, num);
+        }
+};
+
+int main() {
+    PowerOfFour p;
+
+    bool result1 = p.divisionMethod(18);
+    bool result2 = p.multiplicationMethod(1,16);
+
+    if(result1) {
+        cout << "Yes! Its a power of four | Method 1" << endl;
+    } else {
+        cout << "No! Its not a power of four | Method 1" << endl;
+    }
+
+    if(result2) {
+        cout << "Yes! Its a power of four | Method 2" << endl;
+    } else {
+        cout << "No! Its not a power of four | Method 2" << endl;
+    }
+    return 0;
+}
